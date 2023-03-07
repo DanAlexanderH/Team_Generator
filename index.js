1c6601245c42485a9dc22dde5b5c700f54bdfe81
Daniel <ttdanny98@gmail.com>
2023-03-06 17:31:58 -0800

    added code for inquirer and classes for all roles

--- index.js
+++ index.js
@@ -1,6 +1,152 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const Employee = require("./lib/Employee");
+const fullHtml = require("./src/temp")
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFile = util.promisify(fs.appendFile);
+
+groupBlock = [];
+groupString = "";
+
+async function start() {
+    try {
+        await prompt()
+        for (let i=0; i =groupBlock.length; i++) {
+            groupString = groupString + fullHtml.makeCard(groupBlock[i]);
+        }
+
+        let completeHTML = fullHtml.generateHTML(groupString)
+        writeFileAsync("./finish/index.html", completeHTML);
+
+    } catch (err) {
+        return console.log(err);
+    }
+}
+
+
+async function prompt() {
+    let userResponse = "";
+
+    do{
+        try {
+            let question = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is your employee's name? ",
+                    validate: (answer) => {
+                        if(answer === "") {
+                            return "Please enter a valid name"
+                        }
+                        return true
+                    }
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "Enter your employee's # ",
+                    validate: (answer) => {
+                        if(isNaN(answer)) {
+                            return "Please enter a valid ID #"
+                        } 
+                        return true
+                    }
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Enter your employee's email adress. ",
+                    validate: (answer) => {
+                        if(answer === "") {
+                            return "Please enter an email adress."
+                        } 
+                        return true
+                    }
+                },
+                {
+                    type: "list",
+                    name: "role",
+                    message: "What is your employee's role? ",
+                    choices: [
+                        "Engineer",
+                        "Intern",
+                        "Manager"
+                    ]
+                }
+        
+            ]);
+
+            let response = ""
+            if(question.role === "Engineer") {
+                response = await inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "ans",
+                        message: "What is the employee's GitHub Username? ",
+                        validate: (answer) => {
+                            if(answer === "") {
+                                return "Please enter a valid GitHub Username. "
+                            } 
+                            return true
+                        }
+                    }, ]);
+
+                    const engineer = new Engineer(question.name, question.id, question.email, question.ans);
+                    groupBlock.push(engineer);
+
+            } else if (question.role === "Manager") {
+                response = await inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "ans",
+                        message: "What is your employee's office number? ",
+                        validate: (answer) => {
+                            if(isNaN(answer)) {
+                                return "Please enter a office number. "
+                            }
+                            return true
+                        }
+                    }, ]);
+                    
+                    const manager = new Manager(question.name, question.id, question.email, question.ans);
+                    groupBlock.push(manager);
+
+                } else if (question.role === "Intern") {
+                    response = await inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "ans",
+                            message: "What is the name of the school being attended? ",
+                            validate: (answer) => {
+                                if(answer === "") {
+                                    return "Please enter a valid school name. "
+                                } 
+                                return true
+                            }
+                        }, ]);
+                        
+                        const intern = new Intern(question.name, question.id, question.email, question.ans);
+                        groupBlock.push(intern);
+        }
+    } catch (err) {
+        return console.log(err);
+    }
+    userResponse = await inquirer.prompt([
+        {
+            type: "list",
+            name: "finish",
+            message: "Do you want to add another employee? ",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        }, ]);
+    } while (userResponse.finish === "Yes");
+};
+
+
+start();
---
+++ src/temp.js
@@ -0,0 +1,53 @@
+const generateHTML = function(groupString) {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Team Generator</title>
+
+</head>
+<body>
+<h1 id="header">Team Generator</h1>
+<div class="cardCtn">
+    ${groupString}
+</div>
+</body>
+</html> `
+}
+
+const makeCard = function (status) {
+    let icon;
+    let roleInfo;
+
+    if (status.title === "Manager") {
+        icon = `<i class="fa fa-coffee" aria-hidden="true"></i>`
+        roleInfo = `Office Number: ${status.office}`
+    } else if (status.title === "Engineer") {
+        icon = `<i class="fa fa-cogs" aria-hidden="true"></i>`
+        roleInfo = `GitHub: <a href="http://github.com/${status.github}" target="_blank">${status.github}</a>`
+    } else if (status.title === "Intern") {
+        icon = `<i class="fa fa-user" aria-hidden="true"></i>`
+        roleInfo = `School: ${status.school}`
+    }
+
+    return `<div class="card" style="width: 19rem;">
+<div class="card-header">
+    <h2>${status.name}</h2>
+    ${icon}
+    <p>${status.title}</p>    
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">ID: ${status.id}</li>
+  <li class="list-group-item">Email: ${status.email}</li>
+  <li class="list-group-item">${roleInfo}</li>
+</ul>
+</div>`
+}
+
+exports.generateHTML = generateHTML;
+exports.makeCard = makeCard;
