02223f0c17e1cb353304a69550e9d3f882fb7075
Daniel <ttdanny98@gmail.com>
2023-03-07 14:23:53 -0800

    cleaned up html

--- src/temp.js
+++ src/temp.js
@@ -7,13 +7,13 @@ const generateHTML = function(groupString) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-    <link rel="stylesheet" href="./css/style.css">
     <title>Team Generator</title>
     <style>
     h1 {
-        background-color: rgb(178, 180, 182);
+        color: #fff;
+        background-color: rgb(103, 195, 172);
         height: 90px;
-    text-align: center;
+        text-align: center;
     }
     
 </style>
